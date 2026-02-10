c335fcd09fbcbfcc2c2a5390391544e87b828459
seony42 <seony42@nasmedia.co.kr>
2026-02-10 13:25:28 +0900

    chore: v1.0.5

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // AdFit SDK
         .package(
             url: "https://github.com/adfit/adfit-spm",
-            "3.14.7"..<"3.18.7"
+            "3.14.7"..<"3.18.6"
         ),
         // SSP AdMixerMediation SDK
         .package(
@@ -25,8 +25,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdMixerMediationAdFitBinary",
-            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediationAdFit1.0.5-beta.xcframework.zip",
-            checksum: "894ed0479f943b2542848bf608773741024248ec63ebb92c1a022a384a74e66f"
+            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediationAdFit1.0.5.xcframework.zip",
+            checksum: "dcaaa297c9a96e5cccf73aea0cf17ec3584eb9c3e0a0140dc2e9feed1d799e8c"
         ),
         .target(
             name: "iOS_SSP_AdFit_SPM",
